effba516fc656392c223d5c82b85df87ef256e70
Sylvester-Ad <sylvesteradade895@gmail.com>
2025-07-31 12:36:12 +0000

    Test different configs for render

--- build.sh
+++ build.sh
@@ -8,5 +8,3 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 
 python manage.py migrate
-
-DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@email.com DJANGO_SUPERUSER_PASSWORD=adminpass python manage.py createsuperuser --noinput
